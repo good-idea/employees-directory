25bf2f61bb77615d430362878443b15c5bda257f
Joseph Thomas <joseph@good-idea.studio>
2019-07-17 19:31:59 -0700

    fix(server): fix mutations

--- server/src/resolvers/Mutation.ts
+++ server/src/resolvers/Mutation.ts
@@ -2,7 +2,7 @@ import { stringArg } from 'nexus'
 import { prismaObjectType } from 'nexus-prisma'
 
 // @ts-ignore - related issue: https://github.com/prisma/nexus-prisma/issues/291
-export const Query = prismaObjectType({
+export const Mutation = prismaObjectType({
 	name: 'Mutation',
 	definition(t) {
 		t.prismaFields(['*'])
